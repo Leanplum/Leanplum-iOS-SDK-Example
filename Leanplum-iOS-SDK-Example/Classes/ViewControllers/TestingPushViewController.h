18c89b36eb27f20b86a0cb82369243cf79ea9e6a
Ben Marten <ben.marten@me.com>
2018-04-04 10:19:45 -0700

    refactor(nav): refactor nav towards using segue's. add new testing vc.

---
+++ Leanplum-iOS-SDK-Example/Classes/ViewControllers/TestingPushViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestingPushViewController.h
+//  Leanplum-iOS-SDK-Example
+//
+//  Created by Ben Marten on 4/4/18.
+//  Copyright © 2018 Leanplum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestingPushViewController : UIViewController
+
+@end
